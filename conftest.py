507b1cee23a99a2fe4d91efd2af2f404c0ced23c
Alex Cube <af8@af8.ru>
2022-05-01 15:04:03 +0300

    Добавляю использование chromedriver_binary

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+import chromedriver_binary
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -8,15 +9,14 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 def i():
     options = Options()
     options.add_experimental_option("prefs", {
-        "download.default_directory": r"c:\selenium_browsers\download",
+        "download.default_directory": r"download",
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     })
     capabilities = DesiredCapabilities.CHROME
     capabilities['goog:loggingPrefs'] = {'performance': 'ALL'}
-    driver = webdriver.Chrome(executable_path=r"C:\selenium_browsers\chromedriver.exe",
-                              desired_capabilities=capabilities,
+    driver = webdriver.Chrome(desired_capabilities=capabilities,
                               chrome_options=options)
     driver.maximize_window()
     yield driver
